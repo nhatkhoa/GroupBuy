65fe3dba20a841bab438016c9f077765343e68bc
Khoa Hoang <khoahoang@kms.com.vn>
2015-12-19 18:52:55 +0700

    Remove debugbar

--- config/app.php
+++ config/app.php
@@ -145,7 +145,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
-        Barryvdh\Debugbar\ServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
         'Darryldecode\Cart\CartServiceProvider',
 
@@ -197,7 +196,6 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-        'Debugbar'  => Barryvdh\Debugbar\Facade::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Cart' => 'Darryldecode\Cart\Facades\CartFacade',
